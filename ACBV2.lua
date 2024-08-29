42bddb35fec9b38fc86d3cd665cdc415319a67c7
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-08-29 21:43:15 +0800

    n61

--- ACBV2.lua
+++ ACBV2.lua
@@ -15,7 +15,7 @@ task.spawn(function()
 	getgenv().inf = true
 	getgenv().fpsboost = true
 	getgenv().reconnect = true
-	getgenv().raid = true
+	getgenv().raid = false
 	getgenv().blackScreen = false
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/AnimeCard"))()
 end)
