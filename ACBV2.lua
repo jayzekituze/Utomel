67ca03aa59113d5248e04e4f30e4a8c7943b12ef
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-08-31 10:44:53 +0800

    321

--- ACBV2.lua
+++ ACBV2.lua
@@ -10,3 +10,18 @@ task.spawn(function()
 	script_key="wMMFpzXrIqVVzcsvNMfVXptlZsixXXfT";
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
 end)
+
+task.spawn(function()
+    wait(10)
+    print("acb on top")
+	--you can change the false to true
+
+    getgenv().collectPotion = false
+    getgenv().obby = false
+    getgenv().inf = false
+    getgenv().fpsboost = true
+    getgenv().reconnect = true
+    getgenv().raid = false
+    getgenv().blackScreen = false
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/AnimeCard"))()
+end)
