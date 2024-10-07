d2f25fd9d779e61216e0029a00f99e6dc94ab1a4
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-10-07 17:01:15 +0800

    n1356n16n

--- ACBV2.lua
+++ ACBV2.lua
@@ -2,12 +2,15 @@ local osclock = os.clock()
 repeat task.wait(1) until game:IsLoaded()
 print("ACB2")
 
-wait(15)
+wait(10)
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/jayzekituze/Utomel/main/ACBV2.lua"))()
 
 
+
+
+
 task.spawn(function()
-	wait(10)
+	wait(5)
     print("acb on top")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
 end)
@@ -16,9 +19,8 @@ end)
 
 
 task.spawn(function()
-	wait(10)
+	wait(15)
     print("acb on top")
 	script_key="KxYdJFffvFuqMSfsbBDBhKFSAfeCgRKM";
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
 end)
-
