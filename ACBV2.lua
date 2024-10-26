ca995b16f2c09b27a1f129f9c49e8723c888767d
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-10-26 13:27:31 +0800

    n51n5n15

--- ACBV2.lua
+++ ACBV2.lua
@@ -6,18 +6,13 @@ wait(10)
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/jayzekituze/Utomel/main/ACBV2.lua"))()
 
 
-wait(10)
-task.spawn(function()
-	wait(5)
-    print("acb on top")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
-end)
-
-
-wait(10)
 task.spawn(function()
 	wait(10)
     print("acb on top")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
+
+    wait(30)
     script_key="euSIyzooyhNBmchXBlYBVUdqmDvVhdfS";
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
 end)
+
