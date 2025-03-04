6ccb45309a62f8942b5cc291dee04db8d5bfeca2
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2025-03-04 21:04:31 +0800

    n46n

--- ACBV2.lua
+++ ACBV2.lua
@@ -1,17 +1,2 @@
-local osclock = os.clock()
-repeat task.wait(1) until game:IsLoaded()
-print("ACB2")
-
-wait(10)
---loadstring(game:HttpGet("https://raw.githubusercontent.com/jayzekituze/Utomel/main/ACBV2.lua"))()
-
-
-task.spawn(function()
-	wait(1)
-    print("acb on top")
-    getgenv().cancelgetexpeditiongui = true
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
-
-
-end)
-
+getgenv().cancelgetexpeditiongui = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
