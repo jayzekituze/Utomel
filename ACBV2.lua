0ff734e340f13c922246d8614174d52c77576295
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2025-03-04 21:05:04 +0800

    m457m

--- ACBV2.lua
+++ ACBV2.lua
@@ -1,2 +1,6 @@
+local osclock = os.clock()
+repeat task.wait(1) until game:IsLoaded()
+print("ACB2")
+
 getgenv().cancelgetexpeditiongui = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tatsumaki49/main/123/acbw"))()
